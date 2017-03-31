1485b8b9d892440ec0ca1b9fdbcf6b0cb17d62fe
parigutierrez <parivash.gutierrez@gmail.com>
2017-03-31 15:37:10 -0600

    Segundo intento usando objetos-No funciona :'(

--- js/funciones.js
+++ js/funciones.js
@@ -1,30 +1,38 @@
-//alert("Hola Trello"); Prueba de enlaze html con js
+//alert("Hola Trello"); Prueba de enlace html con js
 //Constructor del objeto lista
-function Lista(nombre,tarea){
-  this.nombre=nombreLista;
-  this.tarea=tareadeLista;
+function Lista(){
+
+  this.division = document.createElement("hr");
+
+  this.crearLista = function (){
+    //Método creando la lista:
+      var listaCr = document.createElement('ul'); //Se crea un elemento lista desordenada
+      var tituloLista = document.getElementById('nombreLista').value; //Se accede al dato ingresado por el usuario que será el título de la lista
+      var name = document.createTextNode(tituloLista);//Se crea el nodo de texto (título de la lista)
+      lista.appendChild(name);//Se crea como hijo de la lista el título de la misma
+      document.body.appendChild(listaCr);//Se muestra en el body la lista
+    }
+
+    this.crearBotonTarea = function(){
+      //Método creando un nuevo botón:
+      var btn = document.createElement("button"); // Se crea un nuevo elemento de tipo botón
+      btn.setAttribute("type", "button");
+      btn.setAttribute("onclick", "crearTarea();");
+      var texto = document.createTextNode("Agregar tarea"); // Se asigna el texto que muestra el botón
+      btn.appendChild(texto); // Se agrega como hijo ese texto al botón
+      document.body.appendChild(btn); //Se muestra en el body el botón
+      var idBoton = document.createAttribute("id");//Se crea el atributo id al botón Agregar tarea, para poder darle funcionalidad
+      idBoton.value = "idBoton"; //Se guarda el id en el elemento
+      //console.log(idBoton);  --Prueba de funcionalidad
+      //  crearTarea();
+    }
 }
 
-function crearLista(){
-  //Creando la lista:
-    var lista =document.createElement('ul'); //Se crea un elemento lista desordenada
-    var tituloLista = document.getElementById('nombreLista').value; //Se accede al dato ingresado por el usuario que será el título de la lista
-    var name = document.createTextNode(tituloLista);//Se crea el nodo de texto (título de la lista)
-    lista.appendChild(name);//Se crea como hijo de la lista el título de la misma
-    document.body.appendChild(lista);//Se muestra en el body la lista
+var listas = []; //Arreglo vacío para almacenar las listas
+  function nuevaLista(){
+    var lista = new Lista()
+    listas.push(lista);
 }
-function crearBotonTarea(){
-  //Creando un nuevo botón:
-    var btn = document.createElement("button"); // Se crea un nuevo elemento de tipo botón
-    var texto = document.createTextNode("Agregar tarea"); // Se asigna el texto que muestra el botón
-    btn.appendChild(texto); // Se agrega como hijo ese texto al botón
-    document.body.appendChild(btn); //Se muestra en el body el botón
-    var idBoton = document.createAttribute("id");//Se crea el atributo id al botón Agregar tarea, para poder darle funcionalidad
-    idBoton.value = "idBoton"; //Se guarda el id en el elemento
-    //console.log(idBoton);  --Prueba de funcionalidad
-    crearTarea();
-}
-//document.getElementById("idBoton").onclick = crearTarea();
 
 function crearTarea(){
   var idList = document.createAttribute("id");
@@ -41,6 +49,7 @@ idInput.value = "idInput";
 agregarElementosLista();
 
 }
+
 function agregarElementosLista(){
     var elemento = document.createElement('li');
     var textoLi = document.getElementById('idInput').value;
